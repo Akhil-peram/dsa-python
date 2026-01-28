5dae1ec731992425d8a278040cd5750854a8840e
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-01-28 14:03:09 +0530

    Update max.py

--- max.py
+++ max.py
@@ -1,12 +1,13 @@
-def maxnum(list):
-    if not list:
+def maxnum(arr:list[int]):
+    if not arr:
         return None  # Return None if the list is empty
-    max_value = list[0]
-    for num in list:
+    max_value = arr[0]
+    for num in arr:
         if num > max_value:
             max_value = num
     return max_value
 
 # Example usage
 numbers = [3, 1, 4, 1, 5, 9 , 2, 6, 5]
+
 print(maxnum(numbers))
