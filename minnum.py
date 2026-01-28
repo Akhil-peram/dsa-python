d999ba93e18f1946f911ac236e4d2cd1893a43bb
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-01-28 14:04:01 +0530

    Update minnum.py

--- minnum.py
+++ minnum.py
@@ -1,12 +1,13 @@
-def minnum(list):
-    if not list:
+def minnum(arr:list[int]):
+    if not arr:
         return None  # Return None if the list is empty
-    min_value = list[0]
-    for num in list:
+    min_value = arr[0]
+    for num in arr:
         if num < min_value:
             min_value = num
     return min_value    
 
 # Example usage
 numbers = [3, 1, 4, 1, 5, 9 , 2, 6, 5,0]
+
 print(minnum(numbers))
