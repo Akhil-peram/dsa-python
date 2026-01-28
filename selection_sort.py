42dd7312d0c21837baf0412c6e4b0f10ccf51d73
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-01-28 13:48:21 +0530

    Update selection_sort.py

--- selection_sort.py
+++ selection_sort.py
@@ -1,6 +1,18 @@
 ls = [23,1,2,22,33,5,6,7,2,9,0,34]
 
-def selection(list):
-  pass
+def selection(arr:list[int]):
+  n = len(arr)
+  for i in range(n-1):
+    min_index = i
+    for j in range(i+1,n):
+      if arr[j]<arr[min_index]:
+        min_index=j
+    arr[i],arr[min_index]=arr[min_index],arr[i]
+  return arr
+        
+        
+        
+    
+  
 
 
