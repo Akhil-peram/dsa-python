6f7e87f01001ad84cbbf5cbe907e5086af7516ec
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-10 16:38:41 +0530

    Update quicksort.py

--- quicksort.py
+++ quicksort.py
@@ -1,1 +1,11 @@
+def quicksort(arr:list[int]):
+  if len(arr) <= 1:
+    return arr
+  pivot = arr[len(arr) // 2]
+  left = [x for x in arr if x < pivot ]
+  middle = [x for x in arr if x == pivot ]
+  right = [x for x in arr if x > pivot ]
+  return quicksort(left) + middle + quicksort(right)
 
+arr = [6,2,3,1,9,1,11,10]
+print(quicksort(arr))
