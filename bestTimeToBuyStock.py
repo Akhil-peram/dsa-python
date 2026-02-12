3dc9dd3ae599f347b552b1d69627750696aeb8ca
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-12 17:16:31 +0530

    Update bestTimeToBuyStock.py

--- bestTimeToBuyStock.py
+++ bestTimeToBuyStock.py
@@ -1,1 +1,12 @@
+def maxProfit(prices:list[int]):
+  min_value = float('inf')
+  max_profit = 0
+  for price in prices:
+    if price < min_value:
+      min_value = price
+    elif price - min_value > max_profit:
+      max_profit = price - min_value
+  return max_profit
 
+prices = [3,1,5,6,8,2,4]
+print(maxProfit(prices))
