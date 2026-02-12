9315fdbf177421a5e7f90c7abbdf5a88241ee844
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-12 17:30:32 +0530

    Update Bruteforce_twosum.py

--- Bruteforce_twosum.py
+++ Bruteforce_twosum.py
@@ -1,1 +1,10 @@
-
+# Method 2 Brute Forcing
+def twosum_brute_force(arr, target):
+    for i in range(len(arr)):
+        for j in range(i + 1, len(arr)):
+            if arr[i] + arr[j] == target:
+                return [i, j]
+    return []
+arr = [2,7,11,16]
+target = 9
+print(twosum_brute_force(arr,target))
