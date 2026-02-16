f9a481027dc6979bacb698f9aba7df178d0c136d
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-16 23:37:15 +0530

    Update anagram.py

--- anagram.py
+++ anagram.py
@@ -1,1 +1,8 @@
-
+def isAnagram(self, s: str, t: str) -> bool:
+        so = sorted(s)
+        to = sorted(t)
+        if so == to:
+            return True
+        else:
+            return False
+        
