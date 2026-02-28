8a3cc8eb9eb02e8c7b4acb745d4884c0ff3baf06
Akhil <96526208+Akhil-peram@users.noreply.github.com>
2026-02-28 13:29:45 +0530

    Update valid_parentheses.py

--- valid_parentheses.py
+++ valid_parentheses.py
@@ -1,1 +1,11 @@
-
+def validParentheses(s:str):
+  stack = []
+  bracket_map = {"}":"{,")":"(","]":"["}
+  for char in s:
+    if char != bracket_map:
+      top_element = stack.pop() if stack else '#'
+      if bracket_map[char] != top_element:
+        return False
+    else:
+        stack.append(char)
+  return len(stack) == 0
